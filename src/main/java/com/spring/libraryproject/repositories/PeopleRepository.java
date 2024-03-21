5d2f243a50e42cc909c987b31e27c2ae8908b07c
Michae1-Lee <godzillalee9@gmail.com>
2024-03-22 00:24:13 +0300

    :)

--- src/main/java/com/spring/libraryproject/repositories/PeopleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.spring.libraryproject.repositories;
-import com.spring.libraryproject.models.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PeopleRepository extends JpaRepository<Person, Integer> {
-}
